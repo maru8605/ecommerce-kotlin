b0f4654fa5fb33caddbef97597d15fdb8a2bc22b
maru moreno <maru8605@hotmail.com>
2025-07-20 21:59:43 -0300

    feat(navigation): set up basic navigation with sealed routes and login screen

---
+++ app/src/main/java/com/example/ecommerce_kotlin/ui/navigation/AppNavHost.kt
@@ -0,0 +1,26 @@
+package com.example.ecommerce_kotlin.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.ecommerce_kotlin.ui.login.LoginScreen
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = Screen.Login.route
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(Screen.Login.route) {
+            LoginScreen(navController = navController)
+        }
+
+        // Agregarás más pantallas acá luego
+        // composable(Screen.Register.route) { RegisterScreen(navController) }
+    }
+}
